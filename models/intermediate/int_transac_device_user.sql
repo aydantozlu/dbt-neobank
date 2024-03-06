16cdf72fda3eb1fc4cb0f6be063a0aadfc1ab6ab
Aydan TOZLU <aydan.tozlu@gmail.com>
2024-03-06 08:46:41 +0000

    Modification de la table intermédiaire pour avoir le bon lineage

--- models/intermediate/int_transac_device_user.sql
+++ models/intermediate/int_transac_device_user.sql
@@ -1,14 +1,14 @@
-SELECT
-  t.*,
-  d.brand,
-  u.birth_year,
-  u.country,
-  u.created_date AS user_created_date,
-  u.user_settings_crypto_unlocked,
-  u.plan,
-  u.attributes_notifications_marketing_push,
-  u.attributes_notifications_marketing_email,
-  u.num_contacts
-FROM `neobank1513.dbt_atozlu.stg_raw__transactions` t
-LEFT JOIN `neobank1513.dbt_atozlu.stg_raw__devices` d ON t.user_id = d.user_id
-LEFT JOIN `neobank1513.dbt_atozlu.stg_raw__users` u ON t.user_id = u.user_id
+select
+    t.*,
+    d.brand,
+    u.birth_year,
+    u.country,
+    u.created_date as user_created_date,
+    u.user_settings_crypto_unlocked,
+    u.plan,
+    u.attributes_notifications_marketing_push,
+    u.attributes_notifications_marketing_email,
+    u.num_contacts,
+from {{ ref("stg_raw__transactions") }} t
+left join {{ ref("stg_raw__devices") }} d on t.user_id = d.user_id
+left join {{ ref("stg_raw__users") }} u on t.user_id = u.user_id
