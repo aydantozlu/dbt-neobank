ed07627bd76cf9014ae2e5c0405fbdbe2bdb7c7e
Aydan TOZLU <aydan.tozlu@gmail.com>
2024-03-07 10:16:58 +0000

    Création de la table explo_customer_engagement

---
+++ models/mart/explo_customer_engagement.sql
@@ -0,0 +1,15 @@
+SELECT
+user_id,
+MIN(user_created_date) AS first_user_created_date,
+EXTRACT(YEAR FROM MIN(user_created_date)) AS first_user_created_yr_only,
+EXTRACT(QUARTER FROM MIN(user_created_date)) AS first_user_created_qt_only,
+CONCAT(CAST(EXTRACT(YEAR FROM MIN(user_created_date)) AS STRING), ' Q', CAST(EXTRACT(QUARTER FROM MIN(user_created_date)) AS STRING)) AS acquisition_quarter,
+MAX(created_date) AS last_transaction_date,
+EXTRACT(YEAR FROM MAX(created_date)) AS last_transaction_yr_only,
+EXTRACT(QUARTER FROM MAX(created_date)) AS last_transaction_qt_only,
+CONCAT(CAST(EXTRACT(YEAR FROM MAX(created_date)) AS STRING), ' Q', CAST(EXTRACT(QUARTER FROM MAX(created_date)) AS STRING)) AS last_transaction_quarter,
+DATE_DIFF(DATE('2019-05-16'),DATE(MAX(created_date)),  DAY) AS days_since_last_transaction,
+COUNT(transaction_id) AS nb_transactions,
+SUM(amount_usd) AS amount_transactions
+FROM {{ ref('int_transac_device_user') }}
+GROUP BY user_id
