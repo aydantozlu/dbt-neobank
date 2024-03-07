9e78cb1430bb209dc77135619958a1b04140e487
Aydan TOZLU <aydan.tozlu@gmail.com>
2024-03-07 14:22:47 +0000

    Création de la colonne "churner" et "tranche_age" dans la table "master table"

--- models/intermediate/int_transac_device_user.sql
+++ models/intermediate/int_transac_device_user.sql
@@ -9,8 +9,16 @@ select
     u.attributes_notifications_marketing_push,
     u.attributes_notifications_marketing_email,
     u.num_contacts,
-    m.ea_merchant_mcc_description
+    m.ea_merchant_mcc_description,
+    c.churner,
+    CASE
+        WHEN 2019 - u.birth_year <= 25 THEN '18-25'
+        WHEN 2019 - u.birth_year BETWEEN 26 AND 35 THEN '26-35'
+        WHEN 2019 - u.birth_year BETWEEN 36 AND 50 THEN '36-50'
+        ELSE '+50'
+    END AS tranche_age
 from {{ ref("stg_raw__transactions") }} t
 left join {{ ref("stg_raw__devices") }} d on t.user_id = d.user_id
 left join {{ ref("stg_raw__users") }} u on t.user_id = u.user_id
 left join {{ ref('stg_raw__mcc_description') }} m on t.ea_merchant_mcc = m.ea_merchant_mcc
+left join {{ ref('explo_customer_engagement') }} c on t.user_id = c.user_id
