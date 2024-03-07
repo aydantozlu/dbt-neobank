ca7c86ad0ea433f90c5f6dba6cd9f9a17f8372df
Aydan TOZLU <aydan.tozlu@gmail.com>
2024-03-07 11:26:37 +0000

    Creation de la colonne "Churner" dans notre table "explo_customer_engagement"

--- models/mart/explo_customer_engagement.sql
+++ models/mart/explo_customer_engagement.sql
@@ -10,6 +10,10 @@ EXTRACT(QUARTER FROM MAX(created_date)) AS last_transaction_qt_only,
 CONCAT(CAST(EXTRACT(YEAR FROM MAX(created_date)) AS STRING), ' Q', CAST(EXTRACT(QUARTER FROM MAX(created_date)) AS STRING)) AS last_transaction_quarter,
 DATE_DIFF(DATE('2019-05-16'),DATE(MAX(created_date)),  DAY) AS days_since_last_transaction,
 COUNT(transaction_id) AS nb_transactions,
-SUM(amount_usd) AS amount_transactions
+SUM(amount_usd) AS amount_transactions,
+CASE
+    WHEN DATE_DIFF(DATE('2019-05-16'), DATE(MAX(created_date)), DAY) > 134 THEN 1
+    ELSE 0
+  END AS churner
 FROM {{ ref('int_transac_device_user') }}
 GROUP BY user_id
