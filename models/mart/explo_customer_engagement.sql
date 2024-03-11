c7a03fb49725405d656dd75bfb68e7a4d2169e81
Aydan TOZLU <aydan.tozlu@gmail.com>
2024-03-11 09:49:32 +0000

    Rajout des données RFM + persona dans les tables explo customer et master table

--- models/mart/explo_customer_engagement.sql
+++ models/mart/explo_customer_engagement.sql
@@ -1,5 +1,5 @@
 SELECT
-user_id,
+i.user_id,
 MIN(user_created_date) AS first_user_created_date,
 EXTRACT(YEAR FROM MIN(user_created_date)) AS first_user_created_yr_only,
 EXTRACT(QUARTER FROM MIN(user_created_date)) AS first_user_created_qt_only,
@@ -15,6 +15,14 @@ CASE
     WHEN DATE_DIFF(DATE('2019-05-16'), DATE(MAX(created_date)), DAY) > 134 THEN 1
     ELSE 0
   END AS churner,
-tranche_age
-FROM {{ ref('int_transac_device_user') }}
-GROUP BY user_id, tranche_age
+tranche_age,
+sc.score_f,
+sc.score_r,
+sc.score_m,
+sc.score_rfm,
+CASE WHEN user_settings_crypto_unlocked = 0 THEN 1
+  WHEN user_settings_crypto_unlocked = 1 THEN 2
+  END AS persona
+FROM {{ ref('int_transac_device_user') }} i
+LEFT JOIN {{ ref('stg_raw__user_id_score_rfm') }} sc ON sc.user_id = i.user_id
+GROUP BY user_id, tranche_age, persona, sc.score_f, sc.score_r, sc.score_m, sc.score_rfm
--- models/mart/master_table.sql
+++ models/mart/master_table.sql
@@ -1,4 +1,12 @@
 SELECT i.*,
-c.churner
+c.churner,
+sc.score_f,
+sc.score_r,
+sc.score_m,
+sc.score_rfm,
+CASE WHEN i.user_settings_crypto_unlocked = 0 THEN 1
+  WHEN i.user_settings_crypto_unlocked = 1 THEN 2
+  END AS persona
 FROM {{ ref('int_transac_device_user') }} i 
 LEFT JOIN {{ ref('explo_customer_engagement') }} c ON c.user_id = i.user_id
+LEFT JOIN {{ ref('stg_raw__user_id_score_rfm') }} sc ON sc.user_id = i.user_id
