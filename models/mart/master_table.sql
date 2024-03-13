fc034b151fb1ed37f2cf33816747b8508e28fe08
aydantozlu <aydan.tozlu@gmail.com>
2024-03-13 11:25:46 +0100

    Revert "Merge pull request #20 from aydantozlu/aydan_intermediate"
    This reverts commit e1cae910c4d6e31ee145ac934d1108b34074d267, reversing
    changes made to e9710b4855ceae60d524ee306bed9088a5aebfd1.

--- models/mart/master_table.sql
+++ models/mart/master_table.sql
@@ -7,8 +7,8 @@ sc.score_f,
 sc.score_r,
 sc.score_m,
 sc.score_rfm,
-CASE WHEN i.user_settings_crypto_unlocked = 0 THEN "Chameau"
-  WHEN i.user_settings_crypto_unlocked = 1 THEN "Licorne"
+CASE WHEN i.user_settings_crypto_unlocked = 0 THEN 1
+  WHEN i.user_settings_crypto_unlocked = 1 THEN 2
   END AS persona
 FROM {{ ref('int_transac_device_user') }} i 
 LEFT JOIN {{ ref('explo_customer_engagement') }} c ON c.user_id = i.user_id
